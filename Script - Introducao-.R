f715e399e6d39cd8d8f27081bc221be9d1ae474c
jodavid <jodavid.arts@gmail.com>
2021-02-03 18:45:27 -0300

    atualizacao

--- Script - Introducao-.R
+++ Script - Introducao-.R
@@ -6,26 +6,6 @@
 # Obs.: Serao ocultados os acentos por questao da codificacao
 # <>-------------------------------------------------------------
 # <>-------------------------------------------------------------
-# # fonte: https://gist.github.com/pimentel/256fc8c9b5191da63819
-# head.list <- function(obj, n = 6L, ...)
-# {
-#   stopifnot(length(n) == 1L)
-#   origN <- n
-#   n <- if (n < 0L)
-#     max(length(obj) + n, 0L)
-#   else min(n, length(obj))
-#   lapply(obj[seq_len(n)], function(x)
-#   {
-#     tryCatch({
-#       head(x, origN, ...)
-#     }, error = function(e) {
-#       x
-#     })
-#   })
-# }
-# environment(head.list) <- asNamespace('utils')
-# <>-------------------------------------------------------------
-# <>-------------------------------------------------------------
 #
 # Instalacoes previas necessarias
 # install.packages("devtools")
